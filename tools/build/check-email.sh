2ec87de87c23dbd574b952cbe9993514f43d4bd2
Jose Luis Duran <jlduran@gmail.com>
2023-05-24 03:43:09 +0000

    2 cents

--- tools/build/check-email.sh
+++ tools/build/check-email.sh
@@ -3,7 +3,7 @@
 # Quick check to make sure all emails in the branch are good
 # Anything with noreply in the address
 
-bad=$(git log --prety="%ae" "$@" | grep noreply)
+bad=$(git log --pretty="%ae" "$@" | grep noreply)
 if [ -n "${bad}" ] ; then
     echo "Found the following bad email addresses: ${bad}"
     exit 1
