9b573509cffccd0e2b2f2e3d1c167223f4e02e75
Jose Luis Duran <jlduran@FreeBSD.org>
2025-02-26 03:26:15 +0000

    blocklist: Add vendor import instructions
    Add vendor import instructions for blocklist.
    
    It runs a "freebsd-changes.sh" script that takes care of adapting it to
    FreeBSD.
    
    It also has an extra "freebsd-rename.sh" script that takes care of
    renaming blocklist to blacklist.  Once the naming migration is
    completed, this file should be removed.

---
+++ contrib/blocklist/freebsd-rename.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#################################################
+# Once blacklist has been renamed to blocklist, #
+# this file should be removed.                  #
+#################################################
+
+#
+# Rename blocklist to blacklist
+#
+
+# Change within files blocklist -> blacklist
+files="$(find . -type f -not -iname 'freebsd-*')"
+for file in $files; do
+	sed -i "" -e 's/blocklist/blacklist/g' "$file"
+	sed -i "" -e 's/Blocklist/Blacklist/g' "$file"
+	sed -i "" -e 's/BLOCKLIST/BLACKLIST/g' "$file"
+done
+
+# Rename blocklist -> blacklist
+mv bin/blocklistctl.8 bin/blacklistctl.8
+mv bin/blocklistctl.c bin/blacklistctl.c
+mv bin/blocklistd.8 bin/blacklistd.8
+mv bin/blocklistd.c bin/blacklistd.c
+mv bin/blocklistd.conf.5 bin/blacklistd.conf.5
+mv etc/blocklistd.conf etc/blacklistd.conf
+mv etc/rc.d/blocklistd etc/rc.d/blacklistd
+mv include/blocklist.h include/blacklist.h
+mv lib/blocklist.c lib/blacklist.c
+mv lib/libblocklist.3 lib/libblacklist.3
+mv libexec/blocklistd-helper libexec/blacklistd-helper
