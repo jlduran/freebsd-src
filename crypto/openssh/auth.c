f3b2e2f2a83ebc71e45ea01a1a43bb6282d016b2
Jose Luis Duran <jlduran@FreeBSD.org>
2025-10-12 23:51:03 +0000

    WIP blocklist: Test and upstream these extra probes

--- crypto/openssh/auth.c
+++ crypto/openssh/auth.c
@@ -109,7 +109,7 @@ allowed_user(struct ssh *ssh, struct passwd * pw)
 		return 0;
 
 	if (!options.use_pam && platform_locked_account(pw)) {
-		logit("User %.100s not allowed because account is locked",
+		bl_logit("User %.100s not allowed because account is locked",
 		    pw->pw_name);
 		return 0;
 	}
@@ -124,14 +124,14 @@ allowed_user(struct ssh *ssh, struct passwd * pw)
 		    _PATH_BSHELL : pw->pw_shell); /* empty = /bin/sh */
 
 		if (stat(shell, &st) == -1) {
-			logit("User %.100s not allowed because shell %.100s "
+			bl_logit("User %.100s not allowed because shell %.100s "
 			    "does not exist", pw->pw_name, shell);
 			free(shell);
 			return 0;
 		}
 		if (S_ISREG(st.st_mode) == 0 ||
 		    (st.st_mode & (S_IXOTH|S_IXUSR|S_IXGRP)) == 0) {
-			logit("User %.100s not allowed because shell %.100s "
+			bl_logit("User %.100s not allowed because shell %.100s "
 			    "is not executable", pw->pw_name, shell);
 			free(shell);
 			return 0;
@@ -154,7 +154,7 @@ allowed_user(struct ssh *ssh, struct passwd * pw)
 				fatal("Invalid DenyUsers pattern \"%.100s\"",
 				    options.deny_users[i]);
 			} else if (r != 0) {
-				logit("User %.100s from %.100s not allowed "
+				bl_logit("User %.100s from %.100s not allowed "
 				    "because listed in DenyUsers",
 				    pw->pw_name, hostname);
 				return 0;
@@ -174,7 +174,7 @@ allowed_user(struct ssh *ssh, struct passwd * pw)
 		}
 		/* i < options.num_allow_users iff we break for loop */
 		if (i >= options.num_allow_users) {
-			logit("User %.100s from %.100s not allowed because "
+			bl_logit("User %.100s from %.100s not allowed because "
 			    "not listed in AllowUsers", pw->pw_name, hostname);
 			return 0;
 		}
@@ -182,7 +182,7 @@ allowed_user(struct ssh *ssh, struct passwd * pw)
 	if (options.num_deny_groups > 0 || options.num_allow_groups > 0) {
 		/* Get the user's group access list (primary and supplementary) */
 		if (ga_init(pw->pw_name, pw->pw_gid) == 0) {
-			logit("User %.100s from %.100s not allowed because "
+			bl_logit("User %.100s from %.100s not allowed because "
 			    "not in any group", pw->pw_name, hostname);
 			return 0;
 		}
@@ -192,7 +192,7 @@ allowed_user(struct ssh *ssh, struct passwd * pw)
 			if (ga_match(options.deny_groups,
 			    options.num_deny_groups)) {
 				ga_free();
-				logit("User %.100s from %.100s not allowed "
+				bl_logit("User %.100s from %.100s not allowed "
 				    "because a group is listed in DenyGroups",
 				    pw->pw_name, hostname);
 				return 0;
@@ -205,7 +205,7 @@ allowed_user(struct ssh *ssh, struct passwd * pw)
 			if (!ga_match(options.allow_groups,
 			    options.num_allow_groups)) {
 				ga_free();
-				logit("User %.100s from %.100s not allowed "
+				bl_logit("User %.100s from %.100s not allowed "
 				    "because none of user's groups are listed "
 				    "in AllowGroups", pw->pw_name, hostname);
 				return 0;
@@ -371,7 +371,7 @@ auth_root_allowed(struct ssh *ssh, const char *method)
 		}
 		break;
 	}
-	logit("ROOT LOGIN REFUSED FROM %.200s port %d",
+	bl_logit("ROOT LOGIN REFUSED FROM %.200s port %d",
 	    ssh_remote_ipaddr(ssh), ssh_remote_port(ssh));
 	return 0;
 }
