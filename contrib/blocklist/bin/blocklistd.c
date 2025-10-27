e29ae160364e11cec2d1f67141f9e73dbc70e52f
Jose Luis Duran <jlduran@FreeBSD.org>
2025-10-27 14:48:08 +0000

    blocklist: Minimize differences with upstream
    Commit 4d56eb007b18 ("blocklist: Revert upstream commit ddf6d71")
    manually reverted an upstream change that affects how FreeBSD users
    expect the nfail counter in blocklistd.conf to work.
    
    Attempt to reduce the differences with upstream by re-implementing the
    reverted upstream commit but as a one failure count.
    
    Note that this will add functionality to BLOCKLIST_BAD_USER, which was
    previously not implemented.  At this point, there are no probes in the
    base distribution, nor in the ports tree that make use of this macro.

--- contrib/blocklist/bin/blocklistd.c
+++ contrib/blocklist/bin/blocklistd.c
@@ -222,16 +222,18 @@ process(bl_t bl)
 	switch (bi->bi_type) {
 	case BL_ABUSE:
 		/*
-		 * If the application has signaled abusive behavior,
-		 * set the number of fails to be one less than the
-		 * configured limit.  Fallthrough to the normal BL_ADD
-		 * processing, which will increment the failure count
-		 * to the threshold, and block the abusive address.
+		 * If the application has signaled abusive behavior, set the
+		 * number of fails to be one less than the configured limit.
+		 * Fall through to the normal BL_ADD and BL_BADUSER processing,
+		 * which will increment the failure count to the threshold, and
+		 * block the abusive address.
 		 */
 		if (c.c_nfail != -1)
 			dbi.count = c.c_nfail - 1;
 		/*FALLTHROUGH*/
 	case BL_ADD:
+		/*FALLTHROUGH*/
+	case BL_BADUSER:
 		dbi.count++;
 		dbi.last = ts.tv_sec;
 		if (c.c_nfail != -1 && dbi.count >= c.c_nfail) {
@@ -259,9 +261,6 @@ process(bl_t bl)
 			goto out;
 		dbi.count = 0;
 		dbi.last = 0;
-		break;
-	case BL_BADUSER:
-		/* ignore for now */
 		break;
 	default:
 		(*lfun)(LOG_ERR, "unknown message %d", bi->bi_type);
