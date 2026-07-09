535ddae1520e2c487d0b7280d3ac0db50cace9ad
Jose Luis Duran <jlduran@FreeBSD.org>
2026-07-09 08:25:24 +0000

    Rename the gpt directory to defaults
    SQUASH this commit with the gpt->defaults commit.

--- tools/tools/nanobsd/gpt.sh
+++ tools/tools/nanobsd/gpt.sh
@@ -59,8 +59,8 @@ fi
 
 NANO_BOOTLOADER="boot/gptboot"
 
-NANO_CUST_FILESDIR="${NANO_TOOLS}/gpt/Files"
-NANO_CUST_FILES_MTREE="${NANO_TOOLS}/gpt/Files.mtree"
+NANO_CUST_FILESDIR="${NANO_TOOLS}/defaults/Files"
+NANO_CUST_FILES_MTREE="${NANO_TOOLS}/defaults/Files.mtree"
 
 # Create the /etc/fstab file
 tgt_write_fstab() {
