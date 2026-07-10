f6695c5b34f443b576885e92188487af83d1eb67
Jose Luis Duran <jlduran@FreeBSD.org>
2026-07-10 11:48:19 +0000

    tools: netmap: pkt_hash: Sync with upstream
    Fix ip_hl check
    Due to the missing ntohs, the hash can be erroneously set to zero, which
    breaks load balancing.
    
    fix ipv6h->ip6_ctlun.ip6_un1.ip6_un1_nxt usage
    
    This member of the IPv6 header is only 1 byte and therefore should
    not be byte-swapped using htons [sic].  Using htons [sic] on this value
    means that none of the cases in the switch statement will match and
    the default case will always be chosen.
    
    In addition, the correct ICMP definition for IPv6 should be
    IPPROTO_ICMPV6 instead of IPPROTO_ICMP.
    
    NB: The original commit message mentions "htons", but the actual code
        was mistakenly using "ntohs", as noted by [sic].
    
    Obtained from:	https://github.com/luigirizzo/netmap/commit/75294f31aeab8733ed1cf49450985896c1a94787
    Obtained from:	https://github.com/luigirizzo/netmap/commit/d29ad38c1bbd225c587490f1bcc24346ef39554c

--- tools/tools/netmap/pkt_hash.c
+++ tools/tools/netmap/pkt_hash.c
@@ -149,7 +149,7 @@ decode_ip_n_hash(const struct ip *iph, uint8_t hash_split, uint8_t seed)
 {
 	uint32_t rc = 0;
 
-	if (iph->ip_hl < 5 || iph->ip_hl * 4 > iph->ip_len) {
+	if (iph->ip_hl < 5 || (iph->ip_hl<<2) > ntohs(iph->ip_len)) {
 		rc = 0;
 	} else if (hash_split == 2) {
 		rc = sym_hash_fn(ntohl(iph->ip_src.s_addr),
@@ -231,7 +231,7 @@ decode_ipv6_n_hash(const struct ip6_hdr *ipv6h, uint8_t hash_split, uint8_t seed
 		const struct tcphdr *tcph = NULL;
 		const struct udphdr *udph = NULL;
 
-		switch(ntohs(ipv6h->ip6_ctlun.ip6_un1.ip6_un1_nxt)) {
+		switch(ipv6h->ip6_ctlun.ip6_un1.ip6_un1_nxt) {
 		case IPPROTO_TCP:
 			tcph = (const struct tcphdr *)(ipv6h + 1);
 			rc = sym_hash_fn(ntohl(saddr),
@@ -259,7 +259,7 @@ decode_ipv6_n_hash(const struct ip6_hdr *ipv6h, uint8_t hash_split, uint8_t seed
 		case IPPROTO_GRE:
 			rc = decode_gre_hash((const uint8_t *)(ipv6h + 1), hash_split, seed);
 			break;
-		case IPPROTO_ICMP:
+		case IPPROTO_ICMPV6:
 		case IPPROTO_ESP:
 		case IPPROTO_PIM:
 		case IPPROTO_IGMP:
